0255b501502d299f6f08d7d63ebc3e4374a15e62
juancastillo0 <juancastillo0@users.noreply.github.com>
2020-06-11 05:27:52 +0000

    deploy: 235a3ae4d7909eda0ee25f99030e1d0abbd7a246

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"main.dart.js": "b1c6932280b8face93cb8b05779cd7a4",
+"main.dart.js": "37954c6211f2491056c7d4f09cdc90f6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "7fb4abc2f68b85ccf8536cd8408943c5",
