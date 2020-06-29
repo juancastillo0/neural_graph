a1dcf5d16175e7ef27fb38beda7d4030ec1adf3d
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2020-06-28 20:35:45 -0500

    select input field with dropdown on overflow

--- lib/fields/button_select_field.dart
+++ lib/fields/button_select_field.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget;
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget, useState;
+import 'package:neural_graph/common/extensions.dart';
 
 // @freezed
 // class SelectOption{
@@ -23,17 +26,57 @@ class ButtonSelect<T> extends HookWidget {
   @override
   Widget build(BuildContext ctx) {
     final theme = Theme.of(ctx);
+    final isDropdown = useState(false);
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      print(ctx.size);
+    });
+
+    double buttonTop;
+
+    String _asString(T e) => asString == null ? e.toString() : asString(e);
+
+    if (isDropdown.value) {
+      return Align(
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: DropdownButton<T>(
+            value: selected,
+            items: options.map((e) {
+              final s = _asString(e);
+              return DropdownMenuItem<T>(
+                value: e,
+                child: Text(s),
+              );
+            }).toList(),
+            onChanged: onChange,
+          ),
+        ),
+      );
+    }
     return ButtonBar(
       alignment: MainAxisAlignment.center,
       layoutBehavior: ButtonBarLayoutBehavior.constrained,
       buttonPadding: EdgeInsets.zero,
       children: options.map((e) {
-        final s = asString == null ? e.toString() : asString(e);
+        final s = _asString(e);
+
         return FlatButton(
           key: Key(s),
           onPressed: () => onChange(e),
           color: e == selected ? theme.primaryColor : null,
-          child: Text(s),
+          child: Builder(builder: (ctx) {
+            SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+              print("Text ${ctx.size} ${ctx.globalPaintBounds}");
+              if (buttonTop == null) {
+                buttonTop = ctx.globalPaintBounds.top;
+                return;
+              }
+              if (buttonTop != ctx.globalPaintBounds.top) {
+                isDropdown.value = true;
+              }
+            });
+            return Text(s);
+          }),
         );
       }).toList(),
     );
