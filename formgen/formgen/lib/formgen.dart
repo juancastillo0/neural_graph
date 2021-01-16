100838be1ea51f41843c42d069419032436862fd
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-01-15 21:16:10 -0500

    dependencies changes

--- formgen/formgen/lib/formgen.dart
+++ formgen/formgen/lib/formgen.dart
@@ -1,11 +1,25 @@
 library formgen;
 
 class FormGen {
-  const FormGen({this.allRequired});
+  const FormGen({this.allRequired = false});
 
   final bool allRequired;
 }
 
 class EnumField {
   const EnumField();
+}
+
+class FormInput<T> {
+  const FormInput({
+    this.label,
+    this.description = "",
+    this.width,
+    this.validate,
+  });
+  final String? label;
+  final String description;
+
+  final double? width;
+  final String? Function(T form)? validate;
 }
--- formgen/formgen_generation/lib/src/generator.dart
+++ formgen/formgen_generation/lib/src/generator.dart
@@ -17,11 +17,12 @@ class FormGenGenerator extends GeneratorForAnnotation<FormGen> {
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    // final visitor = ModelVisitor();
+    return "";
+    final visitor = ModelVisitor();
     // final enumVisitor = EnumVisitor();
 
-    // element.visitChildren(visitor);
-    // // final source = StringBuffer();
+    element.visitChildren(visitor);
+    // final source = StringBuffer();
 
     // final paddingElem = visitor.fields["padding"];
     // print(visitor.fields);
@@ -41,13 +42,167 @@ class FormGenGenerator extends GeneratorForAnnotation<FormGen> {
     // // {index: int, values: List<ConvPadding>, same: ConvPadding, valid: ConvPadding, causal: ConvPadding}
     // print(enumVisitor.fields);
 
-    return "// Hey! Annotation found!";
+    final children = visitor.fields.entries.map((entry) {
+      final elem = entry.value.element;
+      final annotation = entry.value.annotation;
+
+      return """\
+tableRow(
+  name: "${annotation.label ?? entry.key}",
+  description: "${annotation.description}",
+  field: ShapeField(
+    field: fields.kernelSize,
+    dimensions: state.dimensions.index,
+  ),
+)
+""";
+    }).join(",");
+    final name = visitor.className!.getDisplayString(withNullability: false);
+
+    return """
+
+class ${name}FormState {
+  ${name}FormState(${name} state) {
+    ${visitor.fields.values.map((e) => """
+    ${e.element.name} = FormFieldValue(
+      get: () => state.${e.element.name},
+      set: (v) => state.${e.element.name} = v,
+    );""").join("\n")}
+  }
+  ${visitor.fields.values.map((e) => 'FormFieldValue<${e.element.type.getDisplayString(withNullability: true)}> ${e.element.name};').join("\n")}
+
+  void dispose() {
+    ${visitor.fields.values.map((e) => '${e.element.name}.dispose();').join("\n")}
+  }
+}
+
+class ConvolutionalForm2 extends HookWidget {
+  const ConvolutionalForm2({
+    Key key,
+    @required this.state,
+  }) : super(key: key);
+
+  final Convolutional state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (ctx) => Table(
+        border: TableBorder.symmetric(
+          inside: const BorderSide(
+            width: 10,
+            style: BorderStyle.none,
+          ),
+        ),
+        columnWidths: const {
+          0: IntrinsicColumnWidth(),
+          1: FixedColumnWidth(40),
+        },
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        children: [
+          tableRow(
+            name: "Dimensions",
+            description: "Number of dimension in the input tensor",
+            field: Observer(builder: (context) {
+              return ButtonSelect<ConvDimensions>(
+                options: ConvDimensions.values,
+                selected: state.dimensions,
+                asString: enumToString,
+                onChange: (v) => state.dimensions = v,
+              );
+            }),
+          ),
+          tableRow(
+            name: "Kernel Size",
+            description: "Size of the filter in each dimension",
+            field: ShapeField(
+              field: fields.kernelSize,
+              dimensions: state.dimensions.index,
+            ),
+          ),
+          tableRow(
+            name: "Padding",
+            description: "Border padding behaviour",
+            field: Observer(
+              builder: (_) => ButtonSelect<ConvPadding>(
+                options: ConvPadding.values,
+                selected: state.padding,
+                onChange: (v) => state.padding = v,
+                asString: enumToString,
+              ),
+            ),
+          ),
+          tableRow(
+            name: "Strides",
+            description: "List of number of omited rows/cols in each dimension",
+            field: ShapeField(
+              field: fields.strides,
+              dimensions: state.dimensions.index,
+            ),
+          ),
+          tableRow(
+            name: "Dilation Rate",
+            description: "List of number of omited rows/cols in each dimension",
+            field: ShapeField(
+              field: fields.dilationRate,
+              dimensions: state.dimensions.index,
+            ),
+          ),
+          tableRow(
+            name: "Use Bias",
+            description: "Use learnable parameter added to the output",
+            field: Align(
+              child: Observer(
+                builder: (_) => Switch(
+                  value: state.useBias,
+                  onChanged: (v) => state.useBias = v,
+                ),
+              ),
+            ),
+          ),
+          tableRow(
+            name: "Separable",
+            description:
+                "Whether the convolution is separated into pointwise and depthwise or full",
+            field: Align(
+              child: Observer(
+                builder: (ctx) => Switch(
+                  value: state.separable,
+                  onChanged: (v) => state.separable = v,
+                ),
+              ),
+            ),
+          ),
+          if (state.separable)
+            tableRow(
+              name: "Depth Multiplier",
+              description: "Expansion rate in a separable convolution",
+              field: TextFormField(
+                inputFormatters: [
+                  WhitelistingTextInputFormatter.digitsOnly,
+                  BlacklistingTextInputFormatter.singleLineFormatter,
+                ],
+                controller: fields.depthMultiplier.controller,
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+          $children,
+        ],
+      ),
+    );
+  }
+}
+""";
   }
 }
 
 class ModelVisitor extends SimpleElementVisitor {
-  DartType className;
-  Map<String, FieldElement> fields = Map();
+  DartType? className;
+  Map<String, _Field> fields = Map();
+
+  final _fieldAnnotation = const TypeChecker.fromRuntime(FormInput);
 
   @override
   dynamic visitConstructorElement(ConstructorElement element) {
@@ -57,13 +212,29 @@ class ModelVisitor extends SimpleElementVisitor {
 
   @override
   dynamic visitFieldElement(FieldElement element) {
-    fields[element.name] = element;
+    if (_fieldAnnotation.hasAnnotationOfExact(element)) {
+      final annotation = _fieldAnnotation.annotationsOfExact(element).first;
+      final _annot = FormInput(
+        description: annotation.getField("description").toStringValue(),
+        label: annotation.getField("label").toStringValue(),
+        validate: (_) =>
+            "", //annotation.getField("validate").toFunctionValue().name,
+        width: annotation.getField("width").toDoubleValue(),
+      );
+      fields[element.name] = _Field(element, _annot);
+    }
     return super.visitFieldElement(element);
   }
 }
 
+class _Field {
+  const _Field(this.element, this.annotation);
+  final FieldElement element;
+  final FormInput annotation;
+}
+
 class EnumVisitor extends SimpleElementVisitor {
-  DartType className;
+  DartType? className;
   Map<String, DartType> fields = Map();
 
   @override
