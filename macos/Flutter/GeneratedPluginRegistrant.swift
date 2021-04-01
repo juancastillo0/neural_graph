54f1db694b7228cad4b906b7a68b1e9f2042ac4d
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-04-01 12:05:00 -0500

    macos directory

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+import FlutterMacOS
+import Foundation
+
+import flutter_webrtc
+import path_provider_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
