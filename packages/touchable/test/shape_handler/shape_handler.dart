0513309dcef8436bf5d366d898432feff16099a7
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-19 16:02:53 +0000

    remove touchable

--- packages/touchable/test/shape_handler/shape_handler.dart
+++
@@ -1,9 +0,0 @@
-import 'hittest_test/defer_to_child.dart';
-import 'hittest_test/opaque.dart';
-import 'hittest_test/translucent.dart';
-
-void testShapeHandler() {
-  testDeferToChild();
-  testOpaque();
-  testTranslucent();
-}
